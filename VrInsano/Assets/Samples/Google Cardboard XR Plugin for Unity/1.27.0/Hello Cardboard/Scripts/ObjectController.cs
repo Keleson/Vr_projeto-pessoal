0f9b95a28dce9bc68ffc3a0203382b7db968f2d5
Nadolny <nadolnyjoaoifpr@gmail.com>
2025-01-11 19:01:07 -0300

    Nadolny commit

--- VrInsano/Assets/Samples/Google Cardboard XR Plugin for Unity/1.27.0/Hello Cardboard/Scripts/ObjectController.cs
+++ VrInsano/Assets/Samples/Google Cardboard XR Plugin for Unity/1.27.0/Hello Cardboard/Scripts/ObjectController.cs
@@ -28,12 +28,13 @@ public class ObjectController : MonoBehaviour
     /// The material to use when this object is inactive (not being gazed at).
     /// </summary>
     public Material InactiveMaterial;
-    public Vector3 speedRotation; 
+    public Material GazedAtMaterial;
+    
 
     /// <summary>
     /// The material to use when this object is active (gazed at).
     /// </summary>
-    public Material GazedAtMaterial;
+    
 
     // The objects are about 1 meter in radius, so the min/max target distance are
     // set so that the objects are always within the room (which is about 5 meters
@@ -46,8 +47,9 @@ public class ObjectController : MonoBehaviour
     private Renderer _myRenderer;
     private Vector3 _startingPosition;
 
-    public bool rotationVeri; 
-
+    
+    public GameObject calabreso;
+    public GameObject calabreso2;
     /// <summary>
     /// Start is called before the first frame update.
     /// </summary>
@@ -59,36 +61,12 @@ public class ObjectController : MonoBehaviour
     }
     public void Update(){
 
-        if (rotationVeri){
-            transform.Rotate(speedRotation * Time.deltaTime); 
-        }
     }
 
     /// <summary>
     /// Teleports this instance randomly when triggered by a pointer click.
     /// </summary>
-    public void TeleportRandomly()
-    {
-        // Picks a random sibling, activates it and deactivates itself.
-        int sibIdx = transform.GetSiblingIndex();
-        int numSibs = transform.parent.childCount;
-        sibIdx = (sibIdx + Random.Range(1, numSibs)) % numSibs;
-        GameObject randomSib = transform.parent.GetChild(sibIdx).gameObject;
-
-        // Computes new object's location.
-        float angle = Random.Range(-Mathf.PI, Mathf.PI);
-        float distance = Random.Range(_minObjectDistance, _maxObjectDistance);
-        float height = Random.Range(_minObjectHeight, _maxObjectHeight);
-        Vector3 newPos = new Vector3(Mathf.Cos(angle) * distance, height,
-                                     Mathf.Sin(angle) * distance);
-
-        // Moves the parent to the new position (siblings relative distance from their parent is 0).
-        transform.parent.localPosition = newPos;
-
-        randomSib.SetActive(true);
-        gameObject.SetActive(false);
-        SetMaterial(false);
-    }
+   
 
     /// <summary>
     /// This method is called by the Main Camera when it starts gazing at this GameObject.
@@ -96,7 +74,7 @@ public class ObjectController : MonoBehaviour
     public void OnPointerEnter()
     {
         SetMaterial(true);
-        rotationVeri = true; 
+        
         
     }
 
@@ -106,7 +84,7 @@ public class ObjectController : MonoBehaviour
     public void OnPointerExit()
     {
         SetMaterial(false);
-        rotationVeri = false; 
+       
 
     }
 
@@ -116,8 +94,10 @@ public class ObjectController : MonoBehaviour
     /// </summary>
     public void OnPointerClick()
     {
-        TeleportRandomly();
-        rotationVeri = false;
+        calabreso.SetActive(true);
+        calabreso2.SetActive(false);
+
+
     }
 
     /// <summary>
